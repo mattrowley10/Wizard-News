ad75884b44be33dde8aff1480561b48de2614457
Matthew Rowley <mattrowley10@gmail.com>
2023-05-04 10:38:13 -0600

    Deployment

--- app.js
+++ app.js
@@ -1,7 +1,77 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'))
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+    res.send(html);
+});
+
+app.get(`/posts/:id`, (req, res)=> {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (post.id){
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+            <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+            </small>
+        </div>
+      </div>
+    </body>
+  </html>`;
+  res.send(html)} else {
+    next()
+  }
+}); 
+app.use((err, req, res, next)=>{
+  const id = req.params.id
+  const post = postBank.find(id)
+  console.error(err)
+  res.send(404).send('Page Not Found')
+})
 
 const PORT = 1337;
 
