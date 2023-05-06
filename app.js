1e9855e7a061866db3eb3f52d4db05719c9ff136
Matthew Rowley <mattrowley10@gmail.com>
2023-05-06 13:39:02 -0600

    Completed w/ bonus

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
+const postList = require("./views/postList")
+const postDetails = require("./views/postDetails")
 const app = express();
 
 app.use(express.static('public'))
@@ -8,70 +10,21 @@ app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            <a href="/posts/${post.id}">${post.title}</a>
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`
-
-    res.send(html);
-});
+  res.send(postList(posts))
+})
 
 app.get(`/posts/:id`, (req, res)=> {
-  const id = req.params.id;
-  const post = postBank.find(id);
-  if (post.id){
-  const html = `<!DOCTYPE html>
-  <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            ${post.title}
-            <small>(by ${post.name})</small>
-          </p>
-            <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-            </small>
-        </div>
-      </div>
-    </body>
-  </html>`;
-  res.send(html)} else {
-    next()
-  }
+  const post = postBank.find(req.params.id);
+  res.send(postDetails(post))
 }); 
+
 app.use((err, req, res, next)=>{
   const id = req.params.id
   const post = postBank.find(id)
   console.error(err)
   res.send(404).send('Page Not Found')
 })
+
 
 const {PORT = 1337} = process.env 
 
